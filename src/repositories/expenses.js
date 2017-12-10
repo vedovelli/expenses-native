3ff6989e2bb7c2ca3b7d36aec19575e048252516
Fabio Vedovelli <vedovelli@gmail.com>
2017-12-10 09:36:54 -0200

    A new method to morph the flat list returned from Firebase into a list with sections, grouping together expenses from the same date.

--- src/repositories/expenses.js
+++ src/repositories/expenses.js
@@ -1,8 +1,12 @@
 
+import database from 'firebase/database' // /src/service/firebase/database.js
 import map from 'lodash/map'
 import reverse from 'lodash/reverse'
-import database from 'firebase/database' // /src/service/firebase/database.js
+import uniq from 'lodash/uniq'
 
+/**
+* Get the flat list from Firebase
+*/
 const list = callback => {
   database.ref('expenses').limitToLast(30).on('value', response => {
     if (response.val() != null) {
@@ -18,6 +22,37 @@ const list = callback => {
   })
 }
 
+/**
+* Get the flat list from Firebase
+* and morph it into sections
+*/
+const listWithSections = callback => {
+  list(expenses => {
+    /**
+    * Morph the list into sections
+    */
+    const allDates = expenses.map(expense => expense.date)
+    const uniqDates = uniq(allDates)
+    const sortedDateList = uniqDates.sort(_sortDates)
+    const finalList = sortedDateList.map(date => _getItemsForDate(expenses, date))
+
+    callback(finalList)
+  })
+}
+
+const _sortDates = (a, b) => {
+  a = a.split('/').reverse().join('')
+  b = b.split('/').reverse().join('')
+  return a > b ? -1 : 1
+}
+
+const _getItemsForDate = (expenses, date) => {
+  const data = expenses.filter(expense => expense.date === date)
+  const total = data.reduce((a, c) => a + parseFloat(c.amount, 10), 0).toFixed(2)
+  return { title: date, data, total }
+}
+
 export default {
-  list
+  list,
+  listWithSections
 }
