24a9ba4abeb7afb32e54494221d0ca35765004a1
Fabio Vedovelli <vedovelli@gmail.com>
2017-12-11 18:55:59 -0200

    Button for navigation removed from Expenses/index.js and included in the form.js

--- src/features/Expenses/index.js
+++ src/features/Expenses/index.js
@@ -1,20 +1,16 @@
 
 import React, { Component } from 'react'
-import { View, Button, Text } from 'react-native'
-// import { NativeRouter, Link } from 'react-router-native'
-// import { Navigation, Card } from 'react-router-navigation'
+import { View, Text } from 'react-native'
 import Form from './form'
 import Header from './header'
 import ExpenseList from './list'
 
 export default class Expenses extends Component {
-  onPress () {}
   render() {
     return (
       <View style={{flex: 1}}>
         <Header/>
-        <Button title="New" onPress={this.onPress} />
-        <ExpenseList/>
+        <Form/>
       </View>
     )
   }
