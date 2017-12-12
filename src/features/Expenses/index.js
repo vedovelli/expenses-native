0820373567bfe20b7a60eca4ea764c8bcd834bc8
Fabio Vedovelli <vedovelli@gmail.com>
2017-12-12 21:09:33 -0200

    Clean up

--- src/features/Expenses/index.js
+++ src/features/Expenses/index.js
@@ -8,7 +8,6 @@ import ExpenseList from './list'
 export default class Expenses extends Component {
   constructor (props) {
     super(props)
-    this.backToList = this.backToList.bind(this)
     this.navigate = this.navigate.bind(this)
     this.state = {
       visibleList: false,
@@ -21,19 +20,13 @@ export default class Expenses extends Component {
       buttonTitle: this.state.visibleList ? 'Lista de Despesas' : 'Incluir Despesa'
     })
   }
-  backToList () {
-    this.setState({
-      visibleList: true,
-      buttonTitle: 'Incluir Despesa'
-    })
-  }
   render() {
     return (
       <View style={{flex: 1}}>
         <Header/>
         <Button title={this.state.buttonTitle} onPress={this.navigate}/>
         {this.state.visibleList ? <ExpenseList/> : null}
-        {!this.state.visibleList ? <Form back={this.backToList}/> : null}
+        {!this.state.visibleList ? <Form/> : null}
       </View>
     )
   }
