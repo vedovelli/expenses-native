5194c3fe27739239beb191a6a40069a6fc24d32f
Fabio Vedovelli <vedovelli@gmail.com>
2017-12-13 08:43:03 -0200

    UI for Login

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
+
 import React, { Component } from 'react'
 import { StyleSheet, View } from 'react-native'
 import Expenses from './src/features/Expenses'
+import Login from './src/features/Auth/Login'
 
 export default class SectionListBasics extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Expenses/>
+        <Login/>
       </View>
     )
   }
---
+++ src/features/Auth/Login.js
@@ -0,0 +1,68 @@
+
+import React, { Component } from 'react'
+import Header from 'layout/header'
+import { Platform,
+  View,
+  Text,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Keyboard,
+  TextInput,
+  Button } from 'react-native'
+
+export default class Login extends Component {
+  constructor (props) {
+    super(props)
+    this.doLogin = this.doLogin.bind(this)
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+  componentDidMount () {
+    emailField.focus()
+  }
+  dismiss () {
+    Keyboard.dismiss()
+  }
+  doLogin () {}
+  render () {
+    return (
+      <View style={{flex: 1}}>
+        <Header/>
+        <TouchableWithoutFeedback onPress={this.dismiss}>
+          <View style={styles.formContainer}>
+            <TextInput
+              ref={e => emailField = e}
+              placeholder="E-mail"
+              style={styles.textInput}
+              onChangeText={(email) => this.setState({email})}
+              value={this.state.email} />
+            <TextInput
+              ref={e => passwordField = e}
+              secureTextEntry={true}
+              placeholder="Password"
+              style={styles.textInput}
+              onChangeText={(password) => this.setState({password})}
+              value={this.state.password} />
+            <Button title="Acessar" onPress={this.doLogin} />
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  formContainer: {
+    flex: 4,
+    padding: 10
+  },
+  textInput: {
+    height: 40,
+    paddingLeft: 10,
+    marginBottom: 5,
+    borderColor: Platform.OS === 'ios' ? '#ccc' : null,
+    borderWidth: Platform.OS === 'ios' ? 1 : null,
+  }
+})
