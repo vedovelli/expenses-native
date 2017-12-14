9b75e15ca6862d81240604996abb3d1c791618fc
Fabio Vedovelli <vedovelli@gmail.com>
2017-12-14 20:16:41 -0200

    S tiny bar at the top of the app displaying bitcoin current value in Brazilian Real

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
 
-import React, { Component } from 'react'
-import { StyleSheet, View } from 'react-native'
-import Expenses from './src/features/Expenses'
-import Login from './src/features/Auth/Login'
+import accounting from 'accounting'
+import cryptocoins from 'cryptocoins'
 import Firebase from 'firebase'
+import React, { Component } from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+import Login from './src/features/Auth/Login'
+import Expenses from './src/features/Expenses'
 
 export default class SectionListBasics extends Component {
   constructor () {
     super()
     this.authListener()
     this.state = {
-      currentView: 'expenses'
+      currentView: 'expenses',
+      bitcoin: ''
     }
+  }
+  componentDidMount () {
+    cryptocoins.foxbit().then(res => {
+      const { last: foxbit } = res.data.ticker_1h.exchanges.FOX
+      this.setState({ bitcoin: accounting.formatMoney(foxbit, "R$", 2, ".", ",") })
+    })
   }
   authListener () {
     Firebase.auth().onAuthStateChanged(user => {
@@ -22,6 +31,7 @@ export default class SectionListBasics extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <Text style={styles.foxbit}>Foxbit Bitcoin: {this.state.bitcoin}</Text>
         {this.state.currentView === 'signin' ? <Login/> : null}
         {this.state.currentView === 'expenses' ? <Expenses/> : null}
       </View>
@@ -30,6 +40,13 @@ export default class SectionListBasics extends Component {
 }
 
 const styles = StyleSheet.create({
+  foxbit: {
+    paddingTop: 2,
+    paddingBottom: 2,
+    textAlign: 'center',
+    color: 'white',
+    backgroundColor: '#2a2a2a'
+  },
   container: {
    flex: 1,
    paddingTop: 22
